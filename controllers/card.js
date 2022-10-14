5f0d2d1e1f8336ec41d2c9005ea56dec8439d7ca
Igor Kolpakov <gragass00@yandex.ru>
2022-10-14 15:53:14 +0300

    fix like and dislike for cards

--- controllers/card.js
+++ controllers/card.js
@@ -52,45 +52,27 @@ module.exports.deleteCards = (req, res) => {
     });
 };
 
-module.exports.likeCard = (res, req) => {
+const handleLike = (req, res, options) => {
+  const action = options.addLike ? '$addToSet' : '$pull';
   Cards.findByIdAndUpdate(
     req.params.cardId,
-    { $addToSet: { likes: req.user._id } },
+    { [action]: { likes: req.user._id } },
     { new: true },
-  )
-    .orFail(new Error('Not Found'))
+  ).then((card) => { res.status(200).send(card); })
     .catch((err) => {
-      if (err.message === 'Not Found') {
-        return res
-          .status(404)
-          .send({ message: 'Карточка с указанным _id не найден' });
-      }
       if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Не корректный _id карточки' });
+        return res.status(404).send({ message: 'Не корректный _id карточки или такой карточки не существует' });
       }
       return res
         .status(500)
-        .send({ message: 'На сервере произошла ошибка' }, err);
+        .send(err);
     });
 };
-module.exports.dislikeCard = (res, req) => {
-  Cards.findByIdAndUpdate(
-    req.params.cardId,
-    { $pull: { likes: req.user._id } },
-    { new: true },
-  )
-    .orFail(new Error('Not Found'))
-    .catch((err) => {
-      if (err.message === 'Not Found') {
-        return res
-          .status(404)
-          .send({ message: 'Карточка с указанным _id не найден' });
-      }
-      if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Не корректный _id карточки' });
-      }
-      return res
-        .status(500)
-        .send({ message: 'На сервере произошла ошибка' }, err);
-    });
+
+module.exports.likeCard = (req, res) => {
+  handleLike(req, res, { addLike: true });
+};
+
+module.exports.dislikeCard = (req, res) => {
+  handleLike(req, res, { addLike: false });
 };
