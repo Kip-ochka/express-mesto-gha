a33c488ec126585d5905024a44813c2a4b12c8fc
Igor Kolpakov <kolpakovigorjob@yandex.ru>
2022-10-16 20:25:02 +0300

    fix after fail push

--- controllers/card.js
+++ controllers/card.js
@@ -54,53 +54,18 @@ const handleLike = (req, res, options) => {
     { [action]: { likes: req.user._id } },
     { new: true },
   )
-<<<<<<< HEAD
-    .orFail(new Error('Not Found'))
-    .catch((err) => {
-      if (err.message === 'Not Found') {
-        return res
-          .status(NOT_FOUND_ERROR_CODE)
-          .send({ message: 'Карточка с указанным _id не найден' });
-=======
     .then((card) => {
       if (!card) {
-        return res.status(404).send({ message: 'Карточка не найдена' });
->>>>>>> 0df3d9df8b966e443ac86fecff362c76ddc7b195
+        return res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Карточка не найдена' });
       }
-      return res.status(200).send(card);
+      return res.send(card);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
         return res.status(INCORRECT_DATA_ERROR_CODE).send({ message: 'Не корректный _id карточки' });
       }
       return res
-<<<<<<< HEAD
         .status(DEFAULT_ERROR_CODE)
-        .send({ message: 'На сервере произошла ошибка' });
-    });
-};
-module.exports.dislikeCard = (res, req) => {
-  Cards.findByIdAndUpdate(
-    req.params.cardId,
-    { $pull: { likes: req.user._id } },
-    { new: true },
-  )
-    .orFail(new Error('Not Found'))
-    .catch((err) => {
-      if (err.message === 'Not Found') {
-        return res
-          .status(NOT_FOUND_ERROR_CODE)
-          .send({ message: 'Карточка с указанным _id не найден' });
-      }
-      if (err.name === 'CastError') {
-        return res.status(INCORRECT_DATA_ERROR_CODE).send({ message: 'Не корректный _id карточки' });
-      }
-      return res
-        .status(DEFAULT_ERROR_CODE)
-        .send({ message: 'На сервере произошла ошибка' });
-    });
-=======
-        .status(500)
         .send({ message: 'Не удалось изменить карточку' });
     });
 };
@@ -111,5 +76,4 @@ module.exports.likeCard = (req, res) => {
 
 module.exports.dislikeCard = (req, res) => {
   handleLike(req, res, { addLike: false });
->>>>>>> 0df3d9df8b966e443ac86fecff362c76ddc7b195
 };
